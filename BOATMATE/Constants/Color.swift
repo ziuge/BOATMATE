12417678f0639cd004f112b85874008baf74a05b
ziuge <xwcjwwd@ewhain.net>
2023-03-28 14:36:03 +0900

    Initial Commit

---
+++ BOATMATE/Constants/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  BOATMATE
+//
+//  Created by CHOI on 2023/03/28.
+//
+
+import UIKit
+
+enum Color {
+    static let gray1 = UIColor(red: 0.971, green: 0.971, blue: 0.971, alpha: 1)
+    static let gray2 = UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1)
+    static let gray3 = UIColor(red: 0.851, green: 0.851, blue: 0.851, alpha: 1)
+    static let gray4 = UIColor(red: 0.592, green: 0.592, blue: 0.592, alpha: 1)
+    static let gray5 = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+    static let gray6 = UIColor(red: 0.149, green: 0.149, blue: 0.149, alpha: 1)
+    
+    static let bgreen = UIColor(red: 0.404, green: 0.835, blue: 0.71, alpha: 1)
+    static let yellow = UIColor(red: 1, green: 0.788, blue: 0.322, alpha: 1)
+    static let red = UIColor(red: 1, green: 0.455, blue: 0.451, alpha: 1)
+    static let green = UIColor(red: 0.69, green: 0.906, blue: 0.369, alpha: 1)
+    static let blue = UIColor(red: 0.345, green: 0.749, blue: 0.976, alpha: 1)
+    static let purple = UIColor(red: 0.737, green: 0.613, blue: 1, alpha: 1)
+    static let pink = UIColor(red: 0.946, green: 0.516, blue: 0.748, alpha: 1)
+    static let brown = UIColor(red: 0.8, green: 0.69, blue: 0.561, alpha: 1)
+}
---
+++ BOATMATE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BOATMATE
+//
+//  Created by CHOI on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
